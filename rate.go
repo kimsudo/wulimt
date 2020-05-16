733a336ded49af893682127fafe1d5562521d1d0
kimsudo <ripvantaro@gmail.com>
2020-05-16 17:35:47 +0800

    Reuse fucn

--- rate.go
+++ rate.go
@@ -20,14 +20,17 @@ func (rl RateLimiter) String() string {
 	return fmt.Sprintf("{limiter:%s, rate:%v/%v}", rl.n, rl.l.Rate.Limit, rl.l.Rate.Period)
 }
 
-// Wait blocks current goroutine to ensure the rate limit requirement is fulfilled.
-func (rl *RateLimiter) Wait() {
+func waitOnLimiter(rl *limiter.Limiter, bufferRate float64) {
 	key := "default"
 	for {
-		ctx, _ := rl.l.Get(context.TODO(), key)
+		ctx, _ := rl.Get(context.TODO(), key)
 		if ctx.Reached {
 			interval := time.Unix(ctx.Reset, 0).Sub(time.Now())
 			if interval > 0 {
+				if bufferRate > 0 {
+					rand, _ := yrand.Float64()
+					interval = time.Duration((bufferRate*rand + 1) * float64(interval))
+				}
 				timer := time.NewTimer(interval)
 				<-timer.C
 			} else {
@@ -39,29 +42,18 @@ func (rl *RateLimiter) Wait() {
 	}
 }
 
+// Wait blocks current goroutine to ensure the rate limit requirement is fulfilled.
+func (rl *RateLimiter) Wait() {
+	waitOnLimiter(rl.l, -1)
+}
+
 const (
 	maxBufferRate = 0.4
 )
 
 // WaitMore blocks current goroutine to ensure the rate limit requirement is fulfilled, and wait a little longer.
 func (rl *RateLimiter) WaitMore() {
-	key := "default"
-	for {
-		ctx, _ := rl.l.Get(context.TODO(), key)
-		if ctx.Reached {
-			interval := time.Unix(ctx.Reset, 0).Sub(time.Now())
-			if interval > 0 {
-				bufferRate, _ := yrand.Float64()
-				interval = time.Duration((bufferRate*maxBufferRate + 1) * float64(interval))
-				timer := time.NewTimer(interval)
-				<-timer.C
-			} else {
-				time.Sleep(400 * time.Millisecond)
-			}
-		} else {
-			break
-		}
-	}
+	waitOnLimiter(rl.l, maxBufferRate)
 }
 
 // Estimate returns average waiting time of the rate limiter.
